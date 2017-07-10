b68820ae788378095f072918cb82159efee75872
Tom <natus.tom@gmail.com>
2017-07-10 13:10:22 +0200

    added gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+
+gulp.task('default', function(){
+    console.log("Hooray - you created a GulpTask.");
+});
+
+gulp.task('html' , function(){
+    console.log("Imagien something usefull being done to your html");
+});
