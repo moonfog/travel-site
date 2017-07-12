c93b6b346ba86fb2be41bf449bfd814feee94bc9
Tom <natus.tom@gmail.com>
2017-07-12 11:08:51 +0200

    added browsersync to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,14 +5,8 @@ var autoprefixer = require('autoprefixer');
 var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
 var cssImport = require('postcss-import');
+var browserSync = require('browser-sync').create();
 
-gulp.task('default', function(){
-    console.log("Hooray - you created a GulpTask.");
-});
-
-gulp.task('html' , function(){
-    console.log("Imagien something usefull being done to your html");
-});
 
 gulp.task('styles' , function(){
    return gulp.src('./app/assets/styles/styles.css')
@@ -21,12 +15,24 @@ gulp.task('styles' , function(){
 });
 
 gulp.task('watch' , function(){
+
+    browserSync.init({
+        server: {
+            baseDir: "app"
+        }        
+    });
+
     watch('./app/index.html', function() {
-        gulp.start('html');
+        browserSync.reload();
     });
 
     watch('./app/assets/styles/**/*.css' , function() {
-        gulp.start('styles');
+        gulp.start('cssInject');
     });
+});
+
+gulp.task('cssInject', ['styles'], function(){
+    return gulp.src('./app/temp/styles/styles.css')
+    .pipe(browserSync.stream());
 });
 
